8dad91ed467c9f06ed0fc7c21a6d24d0907d2ef9
Gulliver Johnson <gulliverjohnson@gmail.com>
2018-06-28 10:23:32 +0100

    Beginning on portal

---
+++ portal/index.js
@@ -0,0 +1,9 @@
+// HANDLES ALL CONNECTION TO MAINFRAME
+
+// Check for stored login
+
+// Request socket to server; send login details if we have them
+
+// On content response, build page - log out if server rejects
+
+// Functions to update statuses
